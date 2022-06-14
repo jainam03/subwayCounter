da0a6a037c8e9116c8fbfbbb89aeb9d84c05e604
Jainam <91124384+jainam03@users.noreply.github.com>
2022-06-14 18:42:52 +0530

    added reset button functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let resetEl = document.getElementById("reset-el")
 let count = 0
 
 function increment() {
@@ -10,8 +11,14 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
-	countEl.textContent = 0
-	count = 0
+    countEl.textContent = 0
+    count = 0
+}
+
+function reset() {
+    count = 0
+    countEl.textContent = 0
+    saveEl.textContent = "Previous entries: "
 }
 
 console.log("Lets count how many people entered the subway")
